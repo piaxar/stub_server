9813600b29a9ca2d545a2af3ec0e3602a6bdca01
piaxar <i.a.panchenko@icloud.com>
2019-03-15 12:35:38 +0300

    Basic stub server

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', function (req, res)  {
+    res.send('Hello there')
+})
+
+app.get('/genres', function (req, res)  {
+    if (req.query.name === 'pop') {
+        res.send({name: 'pop', songs_count: 100})
+    } else if (req.query.name === 'rock') {
+        res.send({name: 'rock', songs_count: 200})
+    } else {
+        res.send({name: 'no_data'})
+    }
+})
+
+app.get('/music', function (req, res)  {
+    if (req.query.username === 'ivan') {
+        res.send([
+            {name: 'Dont stop me now', author: 'Queen'},
+            {name: 'Bohemian Rhapsody', author: 'Queen'}
+        ])
+    } else if (req.query.username === 'oleg') {
+        res.send([
+            {name: 'Cant stop', author: 'Red hot chilli peppers'},
+            {name: 'Snow', author: 'Red hot chilli peppers'}
+        ])
+    } else {
+        res.send([
+            {name: '', author: ''}
+        ])
+    }
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
